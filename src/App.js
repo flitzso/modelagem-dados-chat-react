a5b1bf2146ff5fa63b4fefa713bac966be48633b
flitzso <flitzso@hotmail.com>
2024-06-29 10:54:22 -0300

    Chat react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../src/pages/Home';
+import Profile from '../src/pages/Profile';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
